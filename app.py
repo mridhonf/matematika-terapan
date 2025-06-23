05b2828471e409eb5e32a90e26863398253a6fa6
M Ridho NF <ridhoamstrong4@gmail.com>
2025-06-23 20:17:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -238,7 +238,8 @@ elif menu == "Model Matematika Lainnya":
             final_cost = cost * prediksi
             # Menampilkan hasil prediksi ke user
             st.success(f"Prediksi permintaan untuk tahun {tahun_pred}: {prediksi:.0f} unit")
-            st.success(f"Total Biaya Untuk Tahun {tahun_pred}: Rp.{final_cost})
+            
+            st.success(f"Total Biaya Untuk Tahun {tahun_pred} : Rp. {final_cost}")
 
             # ==========================
             # Visualisasi Garis Regresi
